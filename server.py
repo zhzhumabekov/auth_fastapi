9b583a16bf0222a8120256957bc5d88487163303
Zhumabekov Zhomart <zh.zhumabekov37@gmail.com>
2022-10-17 14:51:22 +0600

    получение POST данных

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import Response
 
 
@@ -10,3 +10,7 @@ def index():
     with open("templates/login.html", "r") as f:
         login_page = f.read()
     return Response(login_page, media_type="text/html")
+
+@app.post("/login")
+def process_login_page(username : str = Form(...), password : str = Form(...)):
+    return Response(f"{username} and {password}", media_type="text/html")
